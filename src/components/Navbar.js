6e27f193c7741c0a35cc73129db90f584973ae3c
jindun619 <jindun619@naver.com>
2023-10-16 02:50:39 +0000

    navbar stickyOnTop & transtion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Link from "next/link";
 
+import { useEffect } from "react";
+
 export default function Navbar() {
+    useEffect(() => {
+        var prevScrollpos = window.scrollY;
+        window.onscroll = function() {
+          var currentScrollPos = window.scrollY;
+          if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".navbar").style.top = "0";
+          } else {
+            document.querySelector(".navbar").style.top = "-100px";
+          }
+          prevScrollpos = currentScrollPos;
+        }
+    })
+
     return (
         <Link href="/">
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 sticky top-0 z-10">
                 <div className="btn btn-ghost normal-case text-xl">가랏 조성몬!(ㅋㅋ)</div>
             </div>
         </Link>
